ba79c0eb45407f0ac7403f3ff5c62fabc02dcdc7
Sarmitha <156922837+sarmi2325@users.noreply.github.com>
2025-10-02 11:48:49 +0530

    Delete resumes directory

--- resumes/urls.py
+++
@@ -1,7 +0,0 @@
-# resumes/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/', views.upload_resume, name='upload_resume'),
-]
--- resumes/views.py
+++
@@ -1,7 +0,0 @@
-# resumes/views.py
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def upload_resume(request):
-    return render(request, 'resumes/upload.html')
