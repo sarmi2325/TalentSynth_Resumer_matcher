d4f530797a3528242d613f324607f4cc719e92f2
Sarmitha <156922837+sarmi2325@users.noreply.github.com>
2025-10-02 14:15:04 +0530

    Update settings.py

--- resume_matcher/settings.py
+++ resume_matcher/settings.py
@@ -23,13 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^@@9wy&ii92thcu9$d$gwcifi)z&df4v5jf*)ji9fj$za%3wy!'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS =['*']
+ALLOWED_HOSTS = [
+    'talentsynthresumermatcher-production.up.railway.app',
+]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://talentsynthresumermatcher-production.up.railway.app",
+]
 
 # Application definition
 INSTALLED_APPS = [
