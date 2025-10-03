f7ad8c69ac822375fd601bc82584e7dfdbcf591a
Sarmitha <156922837+sarmi2325@users.noreply.github.com>
2025-10-03 14:26:19 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('profile/', views.profile, name='profile'),  # upload resume once
     path('update_profile/', views.update_profile, name='update_profile'),
+    path('auto-fill-profile/', views.auto_fill_profile, name='auto_fill_profile'), 
     path('dashboard/', views.dashboard, name='dashboard'),  # upload job description
     path('delete-analysis/<int:analysis_id>/', views.delete_analysis, name='delete_analysis'),  # delete analysis
     path('analysis-details/<int:analysis_id>/', views.get_analysis_details, name='get_analysis_details'),  # get analysis details
