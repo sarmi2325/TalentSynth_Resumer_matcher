45fc0f6cce5cb06c8fc4a4308912579ff8e6e185
Sarmitha <156922837+sarmi2325@users.noreply.github.com>
2025-10-03 16:01:50 +0530

    Update settings.py

--- resume_matcher/settings.py
+++ resume_matcher/settings.py
@@ -76,6 +76,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'resume_matcher.urls'
